4d6d38665f745170085db3c65b831de6425155bd
Алексей Белов <Alenorze@gmail.com>
2017-11-18 14:15:27 +0200

    Goals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount = 4;
+  btnText = 'Add an item';
+  goalText = 'My first goal';
+
   constructor() { }
 
   ngOnInit() {
