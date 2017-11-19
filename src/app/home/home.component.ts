0ca167229a8b5efbc5527fce7be66a77f9eddf5e
Алексей Белов <Alenorze@gmail.com>
2017-11-19 10:53:12 +0200

    List animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+
+        trigger('goals', [
+          transition('* => *', [
+
+            query(':enter', style({ opacity: 0 }), {optional: true}),
+
+            query(':enter', stagger('300ms', [
+              animate('.6s ease-in', keyframes([
+                style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+                style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+              ]))]), {optional: true})
+              ,
+            query(':leave', stagger('300ms', [
+              animate('.6s ease-out', keyframes([
+                style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+                style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+              ]))]), {optional: true})
+          ])
+        ])
+
+      ]
 })
 export class HomeComponent implements OnInit {
 
@@ -24,8 +49,8 @@ export class HomeComponent implements OnInit {
       this.itemCount = this.goals.length;
     }
 
-    // deleteItem() {
-    //   this.goals.delete(this.goalText)
-    // }
+    removeItem(i) {
+      this.goals.splice(i, 1);
+    }
 
   }
